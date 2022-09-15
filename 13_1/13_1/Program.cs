793cb1ffa2a41c08176496740e83c8b946dc486e
grikfel <polytsia15@gmail.com>
2022-09-15 20:35:06 +0300

    13
    13

---
+++ 13_1/13_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, i;
+            Console.Write("Введите натуральное число не меньше двух: ");
+            i = Convert.ToInt32(Console.ReadLine());
+            while (a * a <= i)
+            {
+                if (a % i == 0)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+                a++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
